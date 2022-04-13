70257fc9777814fcf9b6214c2d4e79cc38effa9a
LinnikV <linnikvitek@ukr.net>
2022-04-13 15:11:38 +0300

    header and footer

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#landing_page'
   get 'pricing', to: 'static_pages#pricing'
-  get 'privacy', to: 'static_pages#privasy'
+  get 'privacy', to: 'static_pages#privacy'
   get 'terms', to: 'static_pages#terms'
   
 end
