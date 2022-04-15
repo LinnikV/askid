7f5641546b985618049916c507ec5f38e9c68bfa
LinnikV <linnikvitek@ukr.net>
2022-04-15 13:23:24 +0300

    associate inboxes and user

--- app/models/inbox.rb
+++ app/models/inbox.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Inbox < ApplicationRecord
+  belongs_to :user
   validates :name, presence: true
   validates :name, uniqueness: true
   validates :name, length: { in: 3..200 }
